d1bfd6f09342c8b79991b4bba313a41978a1a07c
JNCJcoder <jorgenettocjunior@gmail.com>
2021-12-28 14:07:02 -0300

    Removido código não utilizado

--- autenticacao/src/routes/authorization.route.ts
+++ autenticacao/src/routes/authorization.route.ts
@@ -5,38 +5,23 @@ import type { SignOptions } from 'jsonwebtoken';
 import JWT from 'jsonwebtoken';
 import basicAuthenticationMiddleware from '../middlewares/basic-authentication.middleware';
 import bearerAuthenticationMiddleware from "../middlewares/bearer-authentication.middleware";
-import ForbiddenError from "../models/errors/forbidden.error.model";
 
 const authorizationRoute = Router();
 
 authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const user = req.user;
+    const user = req.user;
 
-        if (!user) {
-            throw new ForbiddenError('Usuário não informado!');
-        }
+    const jwtPayload = { username: user.username };
+    const jwtOptions: SignOptions = { subject: user?.uuid, expiresIn: '10m' };
 
-        const jwtPayload = { username: user.username };
-        const jwtOptions: SignOptions = { subject: user?.uuid, expiresIn: '10m' };
+    const jwt = JWT.sign(jwtPayload, process.env.SECRET, jwtOptions);
 
-        const jwt = JWT.sign(jwtPayload, process.env.SECRET, jwtOptions);
-
-        res.status(StatusCodes.OK).json({ token: jwt });
-    }
-    catch (error) {
-        next(error);
-    }
+    res.status(StatusCodes.OK).json({ token: jwt });
 });
 
 
 authorizationRoute.post('/token/validate', bearerAuthenticationMiddleware, async (_req: Request, res: Response, next: NextFunction) => {
-    try {
-        res.sendStatus(StatusCodes.OK);
-    }
-    catch (error) {
-        next(error);
-    }
+    res.sendStatus(StatusCodes.OK);
 });
 
 export default authorizationRoute;
